2ecae4fadd227f8ebd1ce5314cec4c5ef58802e5
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-05-28 19:40:22 +0700

    note on `5.1 feature

---
+++ practice/broadcasting.php
@@ -0,0 +1,13 @@
+﻿<?php
+/**
+ * pusher, PubNub very good for real time app
+ * push to event
+ * inform people in room, new comer!!
+ */
+
+/**
+ * broadcasting.php
+ *
+ * pusher wrapped by pusher/pusher-php-server to work with PHP code
+ * WOW, really convenience
+ */
